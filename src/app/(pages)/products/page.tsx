376c2e953cbe87ecf5f8ff889e0ed92e6c059a5a
yucif mustafa <y410902345.za@gmail.com>
2026-05-29 18:37:50 +0300

    Fix API URLs and signup logic

--- src/app/(pages)/products/page.tsx
+++ src/app/(pages)/products/page.tsx
@@ -5,6 +5,7 @@ import SingleCard from "@/app/_components/SingleCard/SingleCard";
 import { ProductType } from "@/types/AllProduct.types";
 import SectionProduct from "@/app/_components/SectionProduct/SectionProduct";
 import ProductsList from "@/app/_components/ProductsList/ProductsList";
+export const dynamic = 'force-dynamic';
 
 export default async function Products() {
   let data = await getAllProducts();
