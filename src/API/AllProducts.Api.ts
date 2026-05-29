1a9105232e74b27a99dc1cf65b8a2c175951f7fd
yucif mustafa <y410902345.za@gmail.com>
2026-05-29 18:21:46 +0300

    Fix layout and page rendering structure

--- src/API/AllProducts.Api.ts
+++ src/API/AllProducts.Api.ts
@@ -1,10 +1,13 @@
-
-
-
-export const getAllProducts = async ()=>{
-     const response = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/products`,{cache:'force-cache'}
+export const getAllProducts = async () => {
+  const response = await fetch(
+    `https://ecommerce.routemisr.com/api/v1/products`, 
+    { cache: 'force-cache' }
   );
+  
+  if (!response.ok) {
+    throw new Error('Failed to fetch products');
+  }
+
   const { data } = await response.json();
-  return data
-}
+  return data;
+};
